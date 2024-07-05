1de941cc69d0b2097eb87e25b634488af1833d00
yb <dydqls5658@naver.com>
2024-07-05 21:43:23 +0900

    20240705

--- Ex_0704/src/main/java/com/korea/first/Ex0704Application.java
+++ Ex_0704/src/main/java/com/korea/first/Ex0704Application.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @Confuguration, @EnableAutoConfiguration, @ComponentScan 세 가지를 하나로 합친 어노테이션
+// @Confuguration : 해당 클래스가 설정파일임을 알려주는 용도
+// @ComponentScan : 컴포넌트 어노테이션이 달린 클래스를 찾아서 메모리에 올려주는 어노테이션
+// @EnableAutoConfiguration : 스프링의 다양한 설정이 자동으로 구성되고 완료됨
 public class Ex0704Application {
 
 	public static void main(String[] args) {
---
+++ Ex_0704/src/main/java/com/korea/first/vo/MemberVO.java
@@ -0,0 +1,13 @@
+package com.korea.first.vo;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@RequiredArgsConstructor
+public class MemberVO {
+	private final String name;
+	private final int age;
+}
